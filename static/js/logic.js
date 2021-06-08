967388ada2c22bcb0d9806a524864b465f6f9cb2
unknown <nguye453@cougars.csusm.edu>
2021-06-08 14:50:06 -0700

    small changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,30 +5,6 @@ d3.json(link).then((data) =>
     createFeatures(data.features);
 });
 
-function getColor(d) {
-    var fillColor = "";
-    if (d < 10) {
-        fillColor = "#89eb34";
-    }
-    else if (d < 30) {
-        fillColor = "#c3eb34";
-    }
-    else if (d < 50) {
-        fillColor = "#ebeb34";
-    }
-    else if (d < 70) {
-        fillColor = "#ebc934";
-    }
-    else if (d < 90) {
-        fillColor = "#eb8934";
-    }
-    else {
-        fillColor = "#eb3434";
-    }
-
-    return fillColor;
-}
-
 function createFeatures(earthquakeData) 
 {
     function onEachFeature(feature, layer) 
@@ -37,7 +13,7 @@ function createFeatures(earthquakeData)
             "</h3><hr><p><u>Occurrence:</u> " + new Date(feature.properties.time) + "</p>" +
             "</h3><p><u>Magnitude:</u> " + feature.properties.mag + "</p>" +
             "</h3><p><u>Depth:</u> " + feature.geometry.coordinates[2] + "</p>");
-            // ADD MORE DATA!!!
+            // ADD MORE DATA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     }
 
     var earthquakes = L.geoJSON(earthquakeData, 
@@ -45,15 +21,15 @@ function createFeatures(earthquakeData)
         onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) 
         {
-            var geojsonMarkerOptions = {
-            radius: (5 * feature.properties.mag),
-            fillColor: getColor(feature.geometry.coordinates[2]),
-            color: "black",
-            weight: 0.5,
-            opacity: 1,
-            fillOpacity: 1
+            var markerStyling = {
+                radius: (5 * feature.properties.mag),
+                fillColor: getColor(feature.geometry.coordinates[2]),
+                color: "black",
+                weight: 0.5,
+                opacity: 1,
+                fillOpacity: 1
             };
-            return L.circleMarker(latlng, geojsonMarkerOptions);
+            return L.circleMarker(latlng, markerStyling);
         }
     });
     
@@ -62,10 +38,10 @@ function createFeatures(earthquakeData)
 
 function createMap(earthquakes) 
 {
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
         maxZoom: 18,
-        id: "mapbox.light",
+        id: "light-v10",
         accessToken: API_KEY
     });
  
@@ -97,4 +73,29 @@ function createMap(earthquakes)
     };
     
     legend.addTo(map);
+}
+
+function getColor(d) {
+    var fillColor = "";
+
+    if (d < 10) {
+        fillColor = "#89eb34";
+    }
+    else if (d < 30) {
+        fillColor = "#c3eb34";
+    }
+    else if (d < 50) {
+        fillColor = "#ebeb34";
+    }
+    else if (d < 70) {
+        fillColor = "#ebc934";
+    }
+    else if (d < 90) {
+        fillColor = "#eb8934";
+    }
+    else {
+        fillColor = "#eb3434";
+    }
+
+    return fillColor;
 }
